1f5249e7db9e45087cdeef9e61c39a5e23fa7392
Alexander Christie <alex@briefly.io>
2016-02-24 22:20:05 +0000

    Implement function analytics
    Signed-off-by: Alexander Christie <alex@briefly.io>

--- src/wrappers/executionTimeMonitor.js
+++ src/wrappers/executionTimeMonitor.js
@@ -1,6 +1,18 @@
+var Keen = require('../datastores/Keen')
+
 module.exports = {
   record: function(function_name, time) {
-    // Stubbed out pending Keen
-    return
+
+    var functionCallEvent = {
+      function: function_name,
+      time: time,
+      keen: {
+        timestamp: new Date().toISOString()
+      }
+    }
+
+    // We literally don't give a single fuck if this call fails
+    Keen.addEvent('execution_times', functionCallEvent)
+
   }
 }
